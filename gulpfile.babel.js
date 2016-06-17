66c1c4bcc0980c6bd69009c9ca2f28ee39b43a68
Quang Lam <Quang Lam>
2016-06-18 02:21:15 +0700

    Lint gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,4 @@
 import gulp from 'gulp';
-import jade from 'gulp-jade';
 import stylus from 'gulp-stylus';
 import rename from 'gulp-rename';
 import bom from 'gulp-bom';
@@ -9,31 +8,31 @@ import path from 'path';
 
 const targetDir = 'platform/www/';
 
-gulp.task('html', () => {
-  return gulp.src('src/index.html')
+gulp.task('html', () =>
+  gulp.src('src/index.html')
     .pipe(bom())
     .pipe(gulp.dest(targetDir))
-})
+);
 
-gulp.task('stylus', () => {
-  return gulp.src(['src/styles/app-dark.styl', 'src/styles/app-light.styl'])
+gulp.task('stylus', () =>
+  gulp.src(['src/styles/app-dark.styl', 'src/styles/app-light.styl'])
     .pipe(stylus({
-      compress: true
+      compress: true,
     }))
     .pipe(rename({
-      suffix: '.min'
+      suffix: '.min',
     }))
     .pipe(bom())
     .pipe(gulp.dest(targetDir))
-})
+);
 
 
-gulp.task('webpack', () => {
-  return gulp.src('src/app.js')
+gulp.task('webpack', () =>
+  gulp.src('src/app.js')
     .pipe(webpackStream({
       cache: true,
       output: {
-        filename: 'bundle.min.js'
+        filename: 'bundle.min.js',
       },
       module: {
         loaders: [
@@ -44,34 +43,34 @@ gulp.task('webpack', () => {
               presets: ['stage-0', 'react'],
             },
             exclude: /(node_modules|bower_components)/,
-          }
-        ]
+          },
+        ],
       },
       resolve: {
-        root: path.resolve('src')
+        root: path.resolve('src'),
       },
       plugins: [
         new webpack.optimize.UglifyJsPlugin({
           sourceMap: false,
-          mangle: false
-        })
-      ]
+          mangle: false,
+        }),
+      ],
     }))
     .pipe(bom())
     .pipe(gulp.dest(targetDir))
-})
+);
 
-gulp.task('winjs', () => {
-  return gulp.src('node_modules/winjs/**/**')
+gulp.task('winjs', () =>
+  gulp.src('node_modules/winjs/**/**')
     .pipe(bom())
     .pipe(gulp.dest(`${targetDir}winjs/`))
-})
+);
 
-gulp.task('winjs-localization', () => {
-  return gulp.src('node_modules/winjs-localization/**/**')
+gulp.task('winjs-localization', () =>
+  gulp.src('node_modules/winjs-localization/**/**')
     .pipe(bom())
     .pipe(gulp.dest(`${targetDir}winjs-localization/`))
-})
+);
 
-gulp.task('prepare', ['winjs', 'winjs-localization'])
-gulp.task('default', ['html', 'stylus', 'webpack'])
+gulp.task('prepare', ['winjs', 'winjs-localization']);
+gulp.task('default', ['html', 'stylus', 'webpack']);
