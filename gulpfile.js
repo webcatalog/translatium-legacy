6e53143c28bccd60ac5fb1e5a07c1082789f0313
Quang Lam <quang.lam2807@gmail.com>
2016-10-05 09:27:12 -0500

    Clean code

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const rename = require('gulp-rename');
@@ -10,13 +12,11 @@ const uglify = require('gulp-uglify');
 const envify = require('envify/custom');
 
 const targetDir = 'platform';
-const liteTargetDir = 'platform-lite';
 
 gulp.task('html', () =>
   gulp.src(['app/index.html', 'app/index.lite.html'])
     .pipe(bom())
     .pipe(gulp.dest(`${targetDir}/www/`))
-    .pipe(gulp.dest(`${liteTargetDir}/www/`))
 );
 
 gulp.task('stylus', () =>
@@ -29,7 +29,6 @@ gulp.task('stylus', () =>
     }))
     .pipe(bom())
     .pipe(gulp.dest(`${targetDir}/www/`))
-    .pipe(gulp.dest(`${liteTargetDir}/www/`))
 );
 
 
@@ -58,59 +57,29 @@ gulp.task('bro', () => {
     .pipe(gulp.dest(`${targetDir}/www/`));
 });
 
-
-gulp.task('bro-lite', () => {
-  const b = browserify({
-    entries: 'app/index.js',
-    debug: false,
-    transform: [
-      babelify.configure({ presets: ['es2015', 'react'] }),
-      envify({
-        NODE_ENV: 'production',
-        APP_PROFILE: 'lite',
-      }),
-    ],
-  });
-
-  return b.bundle()
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(rename({
-      basename: 'bundle',
-      suffix: '.min',
-    }))
-    .pipe(uglify())
-    .pipe(bom())
-    .pipe(gulp.dest(`${liteTargetDir}/www/`));
-});
-
 gulp.task('strings', () =>
   gulp.src('app/strings/**/**')
     .pipe(bom())
     .pipe(gulp.dest(`${targetDir}/strings/`))
-    .pipe(gulp.dest(`${liteTargetDir}/strings/`))
 );
 
 gulp.task('images', () =>
   gulp.src('app/images/**/**')
     .pipe(gulp.dest(`${targetDir}/images/`))
-    .pipe(gulp.dest(`${liteTargetDir}/images/`))
 );
 
 gulp.task('winjs', () =>
   gulp.src('node_modules/winjs/**/**')
     .pipe(bom())
     .pipe(gulp.dest(`${targetDir}/www/winjs/`))
-    .pipe(gulp.dest(`${liteTargetDir}/www/winjs/`))
 );
 
 gulp.task('winjs-localization', () =>
   gulp.src('node_modules/winjs-localization/**/**')
     .pipe(bom())
     .pipe(gulp.dest(`${targetDir}/www/winjs-localization/`))
-    .pipe(gulp.dest(`${liteTargetDir}/www/winjs-localization/`))
 );
 
 gulp.task('prepare', ['strings', 'images', 'winjs', 'winjs-localization']);
-gulp.task('build', ['html', 'stylus', 'bro', 'bro-lite']);
+gulp.task('build', ['html', 'stylus', 'bro']);
 gulp.task('default', ['prepare', 'build']);
