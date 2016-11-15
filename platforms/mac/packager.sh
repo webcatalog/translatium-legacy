99005f9b3d6af887850e0b92bbaaf52567b98a7f
Quang Lam <quang.lam2807@gmail.com>
2016-11-15 13:14:32 -0600

    Improve code sign script

--- platforms/mac/packager.sh
+++ platforms/mac/packager.sh
@@ -1,2 +1,6 @@
-electron-packager ./ "Modern Translator" --app-bundle-id=com.moderntranslator.app --helper-bundle-id=com.moderntranslator.app.helper --app-version=6.0.0 --build-version=6.0.0 --platform=mas --arch=x64 --version=1.4.6 --icon=images/icon.icns --overwrite
-electron-osx-sign "Modern Translator-mas-x64/Modern Translator.app" --verbose
+APP_VERSION=6.0.0
+ELECTRON_VERSION=1.4.6
+
+electron-packager ./ "Modern Translator" --app-bundle-id=com.moderntranslator.app --helper-bundle-id=com.moderntranslator.app.helper --app-version=$APP_VERSION --build-version=$APP_VERSION --platform=mas --arch=x64 --version=$ELECTRON_VERSION --icon=images/icon.icns --overwrite
+electron-osx-sign "Modern Translator-mas-x64/Modern Translator.app" --version=$APP_VERSION
+electron-osx-flat "Modern Translator-mas-x64/Modern Translator.app" --verbose
