2cb39af0b946836fa3684a31252f53eb16eea8b5
Quang Lam <quang.lam2807@gmail.com>
2016-11-17 16:08:50 -0600

    Dev provision profile

--- platforms/mac/packager-dev.sh
+++ platforms/mac/packager-dev.sh
@@ -6,13 +6,9 @@ ELECTRON_VERSION=1.4.6
 APP="Modern Translator"
 # The path of you app to sign.
 APP_PATH="Modern Translator-mas-x64/Modern Translator.app"
-# The path of distribution provisioning profile
-PROVISION_PROFILE_PATH="distribution.provisionprofile"
+# The path of development provisioning profile
+PROVISION_PROFILE_PATH="development.provisionprofile"
 
 electron-packager ./ "Modern Translator" --app-bundle-id=com.moderntranslator.app --helper-bundle-id=com.moderntranslator.app.helper --app-version=$APP_VERSION --build-version=$BUILD_VERSION --platform=mas --arch=x64 --version=$ELECTRON_VERSION --icon=images/icon.icns --overwrite
 
 electron-osx-sign "$APP_PATH" --version=$ELECTRON_VERSION --provisioning-profile=$PROVISION_PROFILE_PATH
-
-electron-osx-flat "$APP_PATH"
-
-codesign --verify --deep --verbose=2 "$APP_PATH"
