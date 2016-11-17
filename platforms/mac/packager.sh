520b170053a162e04b0269594650f478674504c9
Quang Lam <quang.lam2807@gmail.com>
2016-11-17 15:43:06 -0600

    macOS: 6.0.2

--- platforms/mac/packager.sh
+++ platforms/mac/packager.sh
@@ -1,35 +1,16 @@
 APP_VERSION=6.0
-BUILD_VERSION=6.0.1
+BUILD_VERSION=6.0.2
 ELECTRON_VERSION=1.4.6
-
-electron-packager ./ "Modern Translator" --app-bundle-id=com.moderntranslator.app --helper-bundle-id=com.moderntranslator.app.helper --app-version=$APP_VERSION --build-version=$BUILD_VERSION --platform=mas --arch=x64 --version=$ELECTRON_VERSION --icon=images/icon.icns --overwrite
 
 # Name of your app.
 APP="Modern Translator"
 # The path of you app to sign.
 APP_PATH="Modern Translator-mas-x64/Modern Translator.app"
-# The path to the location you want to put the signed package.
-RESULT_PATH="Modern Translator-mas-x64/$APP.pkg"
-# The name of certificates you requested.
-APP_KEY="3rd Party Mac Developer Application: Quang Lam (FVFM95DDPG)"
-INSTALLER_KEY="3rd Party Mac Developer Installer: Quang Lam (FVFM95DDPG)"
 
-FRAMEWORKS_PATH="$APP_PATH/Contents/Frameworks"
-# The path of your plist files.
-CHILD_PLIST="child.plist"
-PARENT_PLIST="parent.plist"
+electron-packager ./ "Modern Translator" --app-bundle-id=com.moderntranslator.app --helper-bundle-id=com.moderntranslator.app.helper --app-version=$APP_VERSION --build-version=$BUILD_VERSION --platform=mas --arch=x64 --version=$ELECTRON_VERSION --icon=images/icon.icns --overwrite
 
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework/Versions/A/Electron Framework"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework/Versions/A/Libraries/libffmpeg.dylib"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework/Versions/A/Libraries/libnode.dylib"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper.app/Contents/MacOS/$APP Helper"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper.app/"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper EH.app/Contents/MacOS/$APP Helper EH"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper EH.app/"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper NP.app/Contents/MacOS/$APP Helper NP"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper NP.app/"
-codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$APP_PATH/Contents/MacOS/$APP"
-codesign -s "$APP_KEY" -f --entitlements "$PARENT_PLIST" "$APP_PATH"
+electron-osx-sign "$APP_PATH" --version=$ELECTRON_VERSION
 
-productbuild --component "$APP_PATH" /Applications --sign "$INSTALLER_KEY" "$RESULT_PATH"
+electron-osx-flat "$APP_PATH"
+
+codesign --verify --deep --verbose=2 "$APP_PATH"
