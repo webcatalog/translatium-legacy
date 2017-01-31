3ede5cda58d987b147d6662b9d29b96a4536985e
Quang Lam <quang.lam2807@gmail.com>
2017-01-30 22:58:26 -0600

    Link to other products

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ $toggle.onclick = function() {
   $toggle.classList.toggle('is-active');
   $menu.classList.toggle('is-active');
 };
+
+var $productToggle = document.querySelector('#nav-product-toggle');
+var $productMenu = document.querySelector('#nav-product-menu');
+
+$productToggle.onclick = function() {
+  $productToggle.classList.toggle('is-active');
+  $productMenu.classList.toggle('is-active');
+};
