44f809da03a040ab1bc0f7913d8679c28576f55d
Quang Lam <quang.lam2807@gmail.com>
2017-02-27 20:15:50 -0600

    Fix GH_TOKEN leak

--- publish.sh
+++ publish.sh
@@ -10,4 +10,4 @@ git checkout -b gh-pages
 git add .
 git -c user.name='Quang Lam' -c user.email='quang.lam2807@gmail.com' commit -m init
 git remote add origin "https://$GH_TOKEN@github.com/modern-translator/website.git"
-git push origin gh-pages -f
+git push origin gh-pages -f --quiet > /dev/null 2>&1
