a3cf1d2bf611ce141d2ab2f7f51c2f4d0f69af3f
Quang Lam <quang.lam2807@gmail.com>
2017-07-09 23:56:51 -0500

    Fixes cordova hook

--- cordova/scripts/after_prepare.js
+++ cordova/scripts/after_prepare.js
@@ -4,16 +4,15 @@ const plist = require('plist');  // www.npmjs.com/package/plist
 
 module.exports = () => {
   const filePath = 'platforms/ios/Modern Translator/Modern Translator-Info.plist';
-
-  // eslint-disable-next-line
-  console.log('Mofifying', filePath, '...');
-  const xml = fs.readFileSync(filePath, 'utf8');
-  const obj = plist.parse(xml);
-
-  obj.CFBundleDisplayName = 'Translator';
-
-  const updatedXml = plist.build(obj);
   if (fs.existsSync(filePath)) {
+    // eslint-disable-next-line
+    console.log('Mofifying', filePath, '...');
+    const xml = fs.readFileSync(filePath, 'utf8');
+    const obj = plist.parse(xml);
+
+    obj.CFBundleDisplayName = 'Translator';
+
+    const updatedXml = plist.build(obj);
     fs.writeFileSync(filePath, updatedXml, { encoding: 'utf8' });
   }
 };
