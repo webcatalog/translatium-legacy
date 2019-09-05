4ff7134979ee70ceb4d524abbfee6e148393580d
Quang Lam <quang.lam2807@gmail.com>
2019-09-05 18:34:38 -0500

    Add Polish localization

--- public/libs/display-languages.js
+++ public/libs/display-languages.js
@@ -11,6 +11,9 @@ const displayLanguages = {
   it: {
     displayName: 'Italiano',
   },
+  pl: {
+    displayName: 'język polski',
+  },
 };
 
 module.exports = displayLanguages;
--- src/constants/display-languages.js
+++ src/constants/display-languages.js
@@ -11,6 +11,9 @@ const displayLanguages = {
   it: {
     displayName: 'Italiano',
   },
+  pl: {
+    displayName: 'język polski',
+  },
 };
 
 export default displayLanguages;
