515bd1a6b2dc200b42699fa4d58be5ce41b44417
Quang Lam <quang.lam2807@gmail.com>
2019-09-05 04:03:33 -0500

    fix

--- src/constants/display-languages.js
+++ src/constants/display-languages.js
@@ -1,4 +1,4 @@
-export default {
+const displayLanguages = {
   en: {
     displayName: 'English',
   },
@@ -6,3 +6,5 @@ export default {
     displayName: 'Tiếng Việt',
   },
 };
+
+export default displayLanguages;
