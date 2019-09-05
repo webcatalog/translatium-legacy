d5326b911e00c261da3556bf47ba9bd1cfa4d912
Quang Lam <quang.lam2807@gmail.com>
2019-09-05 06:48:54 -0500

    activate

--- src/state/root/locale/actions.js
+++ src/state/root/locale/actions.js
@@ -2,12 +2,17 @@ import { UPDATE_LOCALE } from '../../../constants/actions';
 
 import enStrings from '../../../strings/en.json';
 import viStrings from '../../../strings/vi.json';
+import esStrings from '../../../strings/es.json';
 
 export const updateLocale = (langId) => (dispatch) => {
   let strings;
   switch (langId.substring(0, 2)) {
     case 'vi': {
       strings = viStrings;
+      break;
+    }
+    case 'es': {
+      strings = esStrings;
       break;
     }
     default: {
