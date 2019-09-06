4529830d10390b8d5b51e3defb01cb7dd9cf0912
Quang Lam <quang.lam2807@gmail.com>
2019-09-06 00:46:50 -0500

    Add unit testing

--- test/locale.js
+++ test/locale.js
@@ -1,20 +1,18 @@
-/* global describe ity */
+/* global describe it */
 const { expect } = require('chai');
 const fs = require('fs-extra');
 const path = require('path');
 
 describe('All localized languages have valid keys/values', () => {
-  it('should return same list of keys', () => {
-    const defaultKeys = Object.keys(
-      fs.readJSONSync(path.resolve(__dirname, '..', 'src', 'strings', 'en.json')),
-    );
+  const defaultKeys = Object.keys(
+    fs.readJSONSync(path.resolve(__dirname, '..', 'src', 'strings', 'en.json')),
+  );
 
-    const files = fs.readdirSync(path.resolve(__dirname, '..', 'src', 'strings'));
-    files.forEach((file) => {
-      if (!file.endsWith('.json')) return;
+  const files = fs.readdirSync(path.resolve(__dirname, '..', 'src', 'strings'));
+  files.forEach((file) => {
+    if (!file.endsWith('.json')) return;
 
-      console.log(file);
-
+    it(file, () => {
       const keys = Object.keys(
         fs.readJSONSync(path.resolve(__dirname, '..', 'src', 'strings', file)),
       );
