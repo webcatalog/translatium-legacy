575a3456fd03975d7d0578e7400d574913f301e1
Quang Lam <quang.lam2807@gmail.com>
2020-04-30 00:43:50 +0700

    Add search functionality for phrasebook

---
+++ src/components/pages/phrasebook/search-box.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import connectComponent from '../../../helpers/connect-component';
+
+import { loadPhrasebook, updateQuery } from '../../../state/pages/phrasebook/actions';
+
+const styles = (theme) => ({
+  toolbarSearchContainer: {
+    flex: 0,
+    zIndex: 10,
+    position: 'relative',
+    borderRadius: 0,
+  },
+  toolbarSectionSearch: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    height: 40,
+    margin: '0 auto',
+  },
+  searchBarText: {
+    lineHeight: 1.5,
+    padding: '0 4px',
+    flex: 1,
+    userSelect: 'none',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    transform: 'translateY(-1px)',
+    fontWeight: 'normal',
+  },
+  input: {
+    font: 'inherit',
+    border: 0,
+    display: 'block',
+    verticalAlign: 'middle',
+    whiteSpace: 'normal',
+    background: 'none',
+    margin: 0,
+    color: theme.palette.text.primary,
+    width: '100%',
+    paddingLeft: theme.spacing(1),
+    '&:focus': {
+      outline: 0,
+    },
+    '&::placeholder': {
+      color: theme.palette.text.secondary,
+    },
+  },
+  searchButton: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+});
+
+const SearchBox = ({
+  classes,
+  onUpdateQuery,
+  query,
+  onLoadPhrasebook,
+}) => {
+  const clearSearchAction = query.length > 0 && (
+    <>
+      <IconButton
+        color="default"
+        aria-label="Clear"
+        onClick={() => onUpdateQuery('')}
+      >
+        <CloseIcon fontSize="small" className={classes.icon} />
+      </IconButton>
+      <IconButton
+        color="default"
+        aria-label="Search"
+        onClick={() => onLoadPhrasebook(true)}
+      >
+        <KeyboardReturnIcon fontSize="small" className={classes.icon} />
+      </IconButton>
+    </>
+  );
+
+  return (
+    <Paper elevation={1} className={classes.toolbarSearchContainer}>
+      <div className={classes.toolbarSectionSearch}>
+        <Typography
+          className={classes.searchBarText}
+          color="inherit"
+          variant="body1"
+        >
+          <input
+            className={classes.input}
+            onChange={(e) => onUpdateQuery(e.target.value)}
+            onInput={(e) => onUpdateQuery(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && query.length > 0) {
+                onLoadPhrasebook(true);
+              } else if (e.key === 'Escape') {
+                e.target.blur();
+                onUpdateQuery('');
+              }
+            }}
+            placeholder="Search saved translations..."
+            value={query}
+          />
+        </Typography>
+        {clearSearchAction}
+      </div>
+    </Paper>
+  );
+};
+
+SearchBox.defaultProps = {
+  query: '',
+};
+
+SearchBox.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onUpdateQuery: PropTypes.func.isRequired,
+  onLoadPhrasebook: PropTypes.func.isRequired,
+  query: PropTypes.string,
+};
+
+const mapStateToProps = (state) => ({
+  query: state.pages.phrasebook.query,
+});
+
+const actionCreators = {
+  updateQuery,
+  loadPhrasebook,
+};
+
+export default connectComponent(
+  SearchBox,
+  mapStateToProps,
+  actionCreators,
+  styles,
+);
