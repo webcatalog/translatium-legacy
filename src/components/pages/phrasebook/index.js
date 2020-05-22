9e43091d05c528a3b960acaf2136af20a8daa2d0
Quang Lam <quang.lam2807@gmail.com>
2020-05-22 14:53:53 +0700

    remove draggable app bar on macOS

--- src/components/pages/phrasebook/index.js
+++ src/components/pages/phrasebook/index.js
@@ -62,8 +62,6 @@ const styles = (theme) => ({
   appBarColorDefault: {
     background: theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.primary.main,
     color: theme.palette.type === 'dark' ? theme.palette.getContrastText(theme.palette.grey[900]) : theme.palette.primary.contrastText,
-    WebkitAppRegion: window.process.platform === 'darwin' ? 'drag' : undefined,
-    WebkitUserSelect: window.process.platform === 'darwin' ? 'none' : undefined,
   },
   title: {
     flex: 1,
