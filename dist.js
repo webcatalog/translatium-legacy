dc558ea90f628ab2ab189ce00c001c9c1b9b576d
Quang Lam <quang.lam2807@gmail.com>
2021-02-05 15:55:36 +0700

    Update dist.js

--- dist.js
+++ dist.js
@@ -60,7 +60,7 @@ const opts = {
       schemes: ['translatium'],
     },
     appx: {
-      identityName: 'WebCatalogLtd.Translatium-Translator',
+      identityName: 'WebCatalogLtd.TranslatiumTranslator',
       publisher: 'CN=C2673AF2-2F8A-4FAF-AC59-112BBCFB3423',
       backgroundColor: '#43a047',
       languages: Object.keys(displayLanguages),
