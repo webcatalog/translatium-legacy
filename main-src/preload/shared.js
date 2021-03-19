7aa9b17c103cc0e3f746d1035e2727b203b0781c
Quang Lam <quang.lam2807@gmail.com>
2021-03-20 05:59:46 +0700

    Update shared.js

--- main-src/preload/shared.js
+++ main-src/preload/shared.js
@@ -8,10 +8,10 @@ const {
   webFrame,
   desktopCapturer,
 } = require('electron');
-const isDev = require('electron-is-dev');
 const machineId = require('node-machine-id');
 
 // Activate the Sentry Electron SDK as early as possible in every process.
+const isDev = process.env.NODE_ENV !== 'production';
 if (!isDev && ipcRenderer.sendSync('get-preference', 'sentry')) {
   // eslint-disable-next-line global-require
   require('../libs/sentry');
