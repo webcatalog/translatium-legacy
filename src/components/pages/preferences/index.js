454dd494238750309b1f38c9bc4877e46d5dd3a2
Quang Lam <quang.lam2807@gmail.com>
2021-05-11 12:36:49 +0700

    Remove PanMail from cross-promotion

--- src/components/pages/preferences/index.js
+++ src/components/pages/preferences/index.js
@@ -42,7 +42,6 @@ import webcatalogIconPng from '../../../images/products/webcatalog-mac-icon-128@
 import translatiumIconPng from '../../../images/products/translatium-mac-icon-128@2x.png';
 import cloveryIconPng from '../../../images/products/clovery-mac-icon-128@2x.png';
 import singleboxIconPng from '../../../images/products/singlebox-mac-icon-128@2x.png';
-import panmailIconPng from '../../../images/products/panmail-mac-icon-128@2x.png';
 
 const styles = (theme) => ({
   container: {
@@ -547,35 +546,6 @@ const Preferences = (props) => {
                         </Typography>
                         <Typography variant="body2" color="textSecondary" className={classes.promotionSecondaryText}>
                           All Google Apps in One
-                        </Typography>
-                      </div>
-                    </div>
-                  </div>
-                  <ChevronRightIcon color="action" />
-                </ListItem>
-                <Divider />
-                <ListItem
-                  button
-                  onClick={() => {
-                    let url = `https://panmail.app?utm_source=${utmSource}`;
-                    if (window.process.mas) {
-                      url = 'macappstore://apps.apple.com/us/app/panmail/id1551178702';
-                    }
-                    requestOpenInBrowser(url);
-                  }}
-                  className={classes.listItemPromotion}
-                >
-                  <div className={classes.promotionBlock}>
-                    <div className={classes.promotionLeft}>
-                      <img src={panmailIconPng} alt="PanMail" className={classes.appIcon} />
-                    </div>
-                    <div className={classes.promotionRight}>
-                      <div className={classes.promotionRightInner}>
-                        <Typography variant="body1" className={classes.appTitle}>
-                          PanMail
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary">
-                          Cloud-based Email Client
                         </Typography>
                       </div>
                     </div>
