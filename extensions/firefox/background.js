886b17fb81f09fbf2f117b551062ef1ddc97e022
Quang Lam <quang.lam2807@gmail.com>
2021-05-26 20:48:51 +0700

    Add browser extensions (2) (#312)

---
+++ extensions/firefox/background.js
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+/* global browser */
+browser.menus.create({
+  id: 'translate-with-translatium',
+  title: 'Translate with Translatium',
+  contexts: ['selection'],
+}, () => {
+  if (browser.runtime.lastError) {
+    // eslint-disable-next-line no-console
+    console.log(`Got expected error: ${browser.runtime.lastError.message}`);
+  }
+});
+
+browser.menus.onClicked.addListener((o) => {
+  // handle context menu actions
+  if (o && o.selectionText) {
+    browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length < 0) return;
+      const tab = tabs[0];
+      browser.tabs.update(tab.id, {
+        url: `translatium://?text=${encodeURIComponent(o.selectionText)}`,
+      });
+    });
+  }
+});
