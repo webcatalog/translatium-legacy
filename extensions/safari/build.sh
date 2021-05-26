8b8b2fc69076be534acac14770a45aab2f8543e5
Quang Lam <quang.lam2807@gmail.com>
2021-05-26 17:31:21 +0700

    Update build.sh

--- extensions/safari/build.sh
+++ extensions/safari/build.sh
@@ -4,14 +4,14 @@ echo "Building Safari Extension"
 
 CURRENT_DIR=$(dirname "$0")
 
-# build appex
-xcodebuild -project $CURRENT_DIR/translatium/translatium.xcodeproj -alltargets -configuration Release
-
 # download certificates
 wget --content-disposition "$CSC_LINK" -O $CURRENT_DIR/certificates.p12
 
 # import certificates
-security import ./certificates.p12 -P "$CSC_KEY_PASSWORD" -A
+security import $CURRENT_DIR/certificates.p12 -P "$CSC_KEY_PASSWORD" -A
+
+# build appex
+xcodebuild -project $CURRENT_DIR/translatium/translatium.xcodeproj -alltargets -configuration Release
 
 # sign
 codesign --verbose --force -o runtime --sign "3rd Party Mac Developer Application: WebCatalog Ltd" --entitlements $CURRENT_DIR/translatium/safari/safari.entitlements $CURRENT_DIR/translatium/build/Release/safari.appex
