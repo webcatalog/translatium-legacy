f80a02caa964fee929b36c3c54f8c203bffcf9e7
Quang Lam <quang.lam2807@gmail.com>
2021-05-26 16:58:35 +0700

    add safari

---
+++ extensions/safari/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Building Safari Extension"
+
+CURRENT_DIR=$(dirname "$0")
+
+# build appex
+xcodebuild -project $CURRENT_DIR/translatium/translatium.xcodeproj -alltargets -configuration Release
+
+# # download certificates
+# wget --content-disposition "$CSC_LINK" -O $CURRENT_DIR/certificates.p12
+
+# # import certificates
+# security import ./certificates.p12 -P "$CSC_KEY_PASSWORD" -A
+
+# sign
+codesign --verbose --force -o runtime --sign "3rd Party Mac Developer Application: WebCatalog Ltd" --entitlements $CURRENT_DIR/translatium/safari/safari.entitlements $CURRENT_DIR/translatium/build/Release/safari.appex
---
+++ extensions/safari/translatium/safari/Resources/content.js
@@ -0,0 +1,2 @@
+// blank
+// for background.js to work with Safari, content.js needs to be defined
