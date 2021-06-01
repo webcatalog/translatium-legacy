d1fcc1034f6951904d1c58ef9f131ec9ca9d2951
Quang Lam <quang.lam2807@gmail.com>
2021-06-01 16:15:56 +0700

    Unify browser extension codebase (#323)

--- extensions/safari/translatium/safari/Resources/background.js
+++ extensions/safari/translatium/safari/Resources/background.js
@@ -1,7 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-/* global browser */
+
+const browser = window.browser || window.chrome;
+
 browser.menus.create({
   id: 'translate-with-translatium',
   title: browser.i18n.getMessage('extName'),
