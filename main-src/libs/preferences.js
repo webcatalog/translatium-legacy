1936a48b70ba5cd96da6a51012e5c0bde25dfa8a
Quang Lam <quang.lam2807@gmail.com>
2021-08-29 21:39:14 -0500

    Recover legacy rating card preferences state on macOS (#344)

--- main-src/libs/preferences.js
+++ main-src/libs/preferences.js
@@ -65,7 +65,14 @@ const initCachedPreferences = () => {
     settings.setSync('preferenceVersion', 13);
   }
 
-  cachedPreferences = { ...defaultPreferences, ...settings.getSync(`preferences.${v}`) };
+  // ratingCardDidRate2 is only reset on Windows & Linux
+  // so transfer old value (ratingCardDidRate) to new value on macOS
+  const savedPreferences = settings.getSync(`preferences.${v}`);
+  if (process.platform === 'darwin' && !savedPreferences.ratingCardDidRate2 && savedPreferences.ratingCardDidRate) {
+    savedPreferences.ratingCardDidRate2 = true;
+  }
+
+  cachedPreferences = { ...defaultPreferences, ...savedPreferences };
   // backward compatibility
   // es-ES is renamed to es
   if (cachedPreferences.displayLanguage === 'es-ES') {
