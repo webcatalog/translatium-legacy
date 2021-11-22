3448a1789bb2e8e7f8cd6325e6a42cdb552246a8
Quang Lam <quang.lam2807@gmail.com>
2021-11-22 16:30:45 -0600

    Fix electron.remote is undefined (#356)

--- main-src/libs/sentry.js
+++ main-src/libs/sentry.js
@@ -1,12 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-const electron = require('electron');
 const { init } = require('@sentry/electron');
 
 const isRenderer = (process && process.type === 'renderer');
 
 init({
   dsn: process.env.ELECTRON_APP_SENTRY_DSN,
-  release: isRenderer ? electron.remote.app.getVersion() : electron.app.getVersion(),
+  // eslint-disable-next-line global-require
+  release: isRenderer ? require('@electron/remote').app.getVersion() : require('electron').app.getVersion(),
 });
