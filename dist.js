787b7d49652c6c90ac0ec0ef4a4f5198956c36a3
Quang Lam <quang.lam2807@gmail.com>
2021-11-22 22:53:58 -0600

    Fix app icon is not displayed properly on Windows

--- dist.js
+++ dist.js
@@ -74,9 +74,7 @@ const opts = {
     appx: {
       identityName: 'WebCatalogLtd.Translatium',
       publisher: 'CN=C2673AF2-2F8A-4FAF-AC59-112BBCFB3423',
-      backgroundColor: '#43a047',
       languages: Object.keys(displayLanguages),
-      showNameOnTiles: true,
     },
     mac: {
       darkModeSupport: true,
